db29c72ce21e1bef4d6fad29d54df26074aaaf80
Bao Than <109579681+BaoThan@users.noreply.github.com>
2024-02-03 23:22:16 -0800

    Separate GUI app into its own file to avoid error (#2)
    Co-authored-by: Bao Than <b.than@sfsu.edu>

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-import sys
-
 from flask import Flask
 
-from src.gui import run_gui
 from src.routes import *
 
 
 def main() -> None:
-    if len(sys.argv) == 1:
-        app = Flask(__name__)
-        app.register_blueprint(routes)
-        app.run()
-    elif len(sys.argv) == 2 and sys.argv[1] in ("-g", "--gui"):
-        sys.exit(run_gui())
-    else:
-        print("ERROR: Unknown options!")
-        sys.exit(1)
+    app = Flask(__name__)
+    app.register_blueprint(routes)
+    app.run()
 
 
 if __name__ == "__main__":
---
+++ gui_app.py
@@ -0,0 +1,7 @@
+import sys
+
+from src.gui import run_gui
+
+
+if __name__ == "__main__":
+    sys.exit(run_gui())
