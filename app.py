86200243f65899b8a186fb1cd174afdd71cd500a
Bao Than <b.than@sfsu.edu>
2024-02-25 00:26:15 -0800

    Get port from OS env variable

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
 from src.routes import *
 
 
+PORT = os.getenv("PORT", 5000)
+
+
 def main() -> None:
     app = Flask(__name__)
     CORS(app)
+
     app.config["CORS_HEADERS"] = "Content-Type"
     app.register_blueprint(routes)
-    app.run()
+    app.run(port=PORT)
 
 
 if __name__ == "__main__":
